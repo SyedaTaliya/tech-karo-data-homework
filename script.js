3147b6dc32ae65906c781b9120e2b692a31639a1
Syeda-Taliya <49196177+SyedaTaliya@users.noreply.github.com>
2019-07-11 16:07:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+// loops
+// kya chalana ha/kya repeat karna ha
+// kitni bar chalana hai
+// for (let i = 0; i<4;i++) {
+//     console.log("clap!");
+// }
+// for(let count = 100; count > 0;count--){
+//  console.log(count);
+// }
+function result(){
+    let firstValue = document.querySelector(#add).value;
+    let secoundValue = document.querySelector(#add-num).value;
+}
